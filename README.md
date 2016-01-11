72df06a84b55aea49f350d9a3ef0918becd48ff9
fravaccaro <fravaccaro90@gmail.com>
2016-01-11 01:42:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+I created this script to help automating icons resizing for my [theme pack support](https://github.com/fravaccaro/themepacksupport-sailfishos).
+
+## What you need
+
+1. Icons in svg.
+2. A GNU/Linux distro.
+3. ImageMagick (install it via your package manager).
+
+## Usage
+1. Place your icons in `jolla/scalable` if they are Jolla stock icons, `native/scalable` if they are native apps icons and `apk/scalable` if they are Android icons.
+2. Rename your icons accordingly with the [wiki][https://github.com/fravaccaro/themepacksupport-sailfishos/wiki/Icons].
+3. Launch the script.
+4. Go on [packaging your theme](https://github.com/fravaccaro/themepacksupport-sailfishos/wiki).
+
+### Pro tip
+
+You can leave the svg in the final package, I'm looking for a way to enable a more complete icon support in Sailfish OS, so in case they will be already there.
---
+++ themepack-helper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+#    Theme pack support for Sailfish OS - Enables theme pack support in Sailfish OS.
+#    Copyright (C) 2015  fravaccaro fravaccaro90@gmail.com
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Usage
+# Place some icons in scalable folders and launche this script from the root folder of the themepack.
+
+# Resize Jolla stock icons
+if [ "$(ls $jolla)" ]; then
+mogrify -path jolla/z1.0/icons/ -format png -background none -resize 86x86 jolla/scalable/*.svg
+mogrify -path jolla/z1.5/icons/ -format png -background none -resize 129x129 jolla/scalable/*.svg
+mogrify -path jolla/z2.0/icons/ -format png -background none -resize 172x172 jolla/scalable/*.svg
+fi
+
+# Resize native apps icons
+if [ "$(ls $native)" ]; then
+mogrify -path native/86x86/apps/ -format png -background none -resize 86x86 native/scalable/apps/*.svg
+mogrify -path native/108x108/apps/ -format png -background none -resize 108x108 native/scalable/apps/*.svg
+mogrify -path native/128x128/apps/ -format png -background none -resize 128x128 native/scalable/apps/*.svg
+mogrify -path native/256x256/apps/ -format png -background none -resize 256x256 native/scalable/apps/*.svg
+fi
+
+# Resize Android icons
+if [ "$(ls $apk)" ]; then
+mogrify -path apk/86x86 -format png -background none -resize 86x86 apk/scalable/*.svg
+mogrify -path apk/128x128 -format png -background none -resize 128x128 apk/scalable/*.svg
+fi
+
+exit 0
